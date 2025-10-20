371886fa2ecd78181e17f2aa3e916de2f676abe2
heath <heatherm.anderson@mail.utoronto.ca>
2025-10-20 10:15:50 -0400

    m

--- src/main/java/interface_adapter/logout/LogoutController.java
+++ src/main/java/interface_adapter/logout/LogoutController.java
@@ -1,5 +1,6 @@
 package interface_adapter.logout;
 
+import use_case.login.LoginInputData;
 import use_case.logout.LogoutInputBoundary;
 
 /**
@@ -11,6 +12,7 @@ public class LogoutController {
 
     public LogoutController(LogoutInputBoundary logoutUseCaseInteractor) {
         // TODO: Save the interactor in the instance variable.
+        this.logoutUseCaseInteractor = logoutUseCaseInteractor;
     }
 
     /**
@@ -18,5 +20,6 @@ public class LogoutController {
      */
     public void execute() {
         // TODO: run the use case interactor for the logout use case
+        logoutUseCaseInteractor.execute();
     }
 }
