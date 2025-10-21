b0e6f27538019475d70a3381dd095c0e743f6c1e
Kelly He <hekelly3212006@gmail.com>
2025-10-20 20:19:33 -0400

    bug fixes with logout presenter, implemented interactor, added LogOutUseCase to main

--- src/main/java/interface_adapter/logout/LogoutController.java
+++ src/main/java/interface_adapter/logout/LogoutController.java
@@ -18,7 +18,6 @@ public class LogoutController {
      * Executes the Logout Use Case.
      */
     public void execute() {
-        // TODO: run the use case interactor for the logout use case
         logoutUseCaseInteractor.execute();
     }
 }
